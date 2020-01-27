b541994c231d8e17263f7af9ac8371a2157ec28c
Max Spiegelhoff <maspiegelhoff@wpi.edu>
2020-01-27 14:18:26 -0500

    added name correctly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,5 +12,6 @@ public class Main {
 	System.out.println("This is a test of branching");
         System.out.println("Tabby");
         System.out.println("JR is Me");
+        System.out.println("Max is here!")
     }
 }
